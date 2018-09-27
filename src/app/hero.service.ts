a0a1ec7a78f62199d941b1a29b20d7f4c0bd64a8
Min Suk Kim <minkimcodes@gmail.com>
2018-09-27 17:35:33 -0400

    get heroes added

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-
+import { hero } from './hero';
+import { HEROES } from './mock-heroes';
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
   constructor() { }
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
 }
